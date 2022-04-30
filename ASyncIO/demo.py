84ca903d1b6f983669a9f07e35315c487fa78118
jerome <jerome.lzh@gmail.com>
2022-04-30 21:24:38 +0800

    add newbie sample

---
+++ ASyncIO/demo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   demo.py
+@Time    :   2022/04/30 13:11:00
+@Author  :   Jerome
+@Version :   1.0
+@Contact :   jerome.lzh@gmail.com
+@License :   GPL-3.0
+@Desc    :   None
+'''
+
+# here put the import lib
+import asyncio
